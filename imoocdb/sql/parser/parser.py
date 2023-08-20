8a972682188c5631b32998a4b86b8091f31a3d9b
Wotchin <wotchin-db@imooc.cn>
2023-08-21 00:11:31 +0800

    ch(3.5): bugfix for select statements

--- imoocdb/sql/parser/parser.py
+++ imoocdb/sql/parser/parser.py
@@ -13,14 +13,14 @@ class SQLLexer(sly.Lexer):
     tokens = {
         # DDL
         CREATE, DROP,
-        DATABASE, TABLE, INDEX
+        DATABASE, TABLE, INDEX,
 
         # others
-        EXPLAIN
+        EXPLAIN,
 
         # select
-        SELECT, STAR, FROM, WHERE, GROUP_BY, ORDER_BY,
-        JOIN, FULL, INNER, OUTER, LEFT, RIGTH, ON,
+        SELECT, STAR, FROM, WHERE, GROUP_BY, ORDER_BY, ASC, DESC,
+        JOIN, FULL, INNER, OUTER, LEFT, RIGHT, ON,
 
         # DML: INSERT, UPDATE, DELETE
         INSERT, DELETE, INTO, VALUES, UPDATE, SET,
@@ -34,7 +34,7 @@ class SQLLexer(sly.Lexer):
 
         # data type
         ID,
-        INTERGER, QUOTE_STRING, DQUOT_STRING, NULL,
+        INTEGER, QUOTE_STRING, DQUOTE_STRING, NULL,
     }
 
     CREATE = 'CREATE'
@@ -53,6 +53,8 @@ class SQLLexer(sly.Lexer):
     WHERE = 'WHERE'
     GROUP_BY = 'GROUP BY'
     ORDER_BY = 'ORDER BY'
+    ASC = 'ASC'
+    DESC = 'DESC'
     JOIN = 'JOIN'
     FULL = 'FULL'
     INNER = 'INNER'
@@ -93,18 +95,19 @@ class SQLLexer(sly.Lexer):
         return t
 
     QUOTE_STRING = r"'[^']*'"
-    DQUOT_STRING = r'"[^"]*"'
+    DQUOTE_STRING = r'"[^"]*"'
 
 
 # select a, b from t1;
 # select a, b from t1 where a > c;
 # select a, b from t1 where a > c order by b;
 # select a, count(a) from t1 group by a where b > 100;
-from .ast import Select, Star
+from imoocdb.sql.parser.ast import *
 
 class SQLParser(sly.Parser):
-    # select *;  -> Select([Star()])
+    tokens = SQLLexer.tokens
 
+    # select *;  -> Select([Star()])
     @_('SELECT target_columns')
     def select(self, p):
         return Select(targets=p.target_columns)
@@ -112,6 +115,7 @@ class SQLParser(sly.Parser):
     @_('target_columns COMMA target_column')
     def target_columns(self, p):
         p.target_columns.append(p.target_column)
+        return p.target_columns
 
     @_('target_column')
     def target_columns(self, p):
@@ -128,9 +132,212 @@ class SQLParser(sly.Parser):
     @_('expr',
        'function')
     def target_column(self, p):
-        return p.p[0]
+        return p[0]
+
+    # ===== 上面是，带领大家一起实现的部分 =====
+    # ===== 由于时间关系，下面是补充其他类似的部分 ====
+    @_('id LPAREN expr_list_or_nothing RPAREN')
+    def function(self, p):
+        args = p.expr_list_or_nothing
+        if not args:
+            args = []
+        return FunctionOperation(op=p.id, args=args)
+
+    # from 子句
+    @_('select FROM from_table',
+       'select FROM cross_join_tables',
+       'select FROM join_tables')
+    def select(self, p):
+        select = p.select
+        if select.from_table:
+            raise SyntaxError('duplicated from clause.')
+
+        select.from_table = p[2]
+        return select
+
+    @_('identifier')
+    def from_table(self, p):
+        return p.identifier
+
+    # where 子句
+    @_('select WHERE expr')
+    def select(self, p):
+        select = p.select
+        if select.where:
+            raise SyntaxError('duplicated where clause.')
+        if not select.from_table:
+            raise SyntaxError('not set from clause.')
+
+        where_expr = p.expr
+        if not isinstance(where_expr, Operation):
+            raise SyntaxError(
+                f"Require an operation for WHERE clause.")
+        select.where = where_expr
+        return select
+
+    # order by 子句
+    @_('select ORDER_BY ordering_term')
+    def select(self, p):
+        select = p.select
+        if select.order_by:
+            raise SyntaxError('duplicated order by clause.')
+        if not select.from_table:
+            raise SyntaxError('not set from clause.')
+        select.order_by = p.ordering_term
+        return select
+
+    @_('identifier DESC')
+    def ordering_term(self, p):
+        return OrderBy(column=p.identifier, direction='DESC')
+
+    @_('identifier',
+       'identifier ASC')
+    def ordering_term(self, p):
+        return OrderBy(column=p.identifier, direction='ASC')
+
+    # Group by 子句
+    @_('select GROUP_BY expr_list')
+    def select(self, p):
+        select = p.select
+        if select.group_by:
+            raise SyntaxError('duplicated group by clause.')
+        if not select.from_table:
+            raise SyntaxError('not set from clause.')
+
+        group_by = p.expr_list
+        if not isinstance(group_by, list):
+            group_by = [group_by]
+
+        select.group_by = group_by
+        return select
+
+    # Join 子句
+    @_('from_table join_clause from_table')
+    def join_tables(self, p):
+        return Join(left=p[0],
+                    right=p[2],
+                    join_type=p.join_clause)
+
+    @_(JoinType.LEFT_JOIN,
+       JoinType.RIGHT_JOIN,
+       JoinType.INNER_JOIN,
+       JoinType.FULL_JOIN,
+       )
+    def join_clause(self, p):
+        return f'{p[0]} {p[1]}'
+
+    # 也是隐式的join类型，如 select * from t1, t2;
+    @_('from_table COMMA from_table')
+    def cross_join_tables(self, p):
+        return Join(left=p[0],
+                    right=p[2],
+                    join_type=JoinType.CROSS_JOIN)
+
+    # 二元表达式
+    @_('expr EQ expr',
+       'expr NE expr',
+       'expr GEQ expr',
+       'expr GT expr',
+       'expr LEQ expr',
+       'expr LT expr',
+       'expr AND expr',
+       'expr OR expr',)
+    def expr(self, p):
+        return BinaryOperation(op=p[1], args=(p.expr0, p.expr1))
+
+    # 表达式
+    @_('expr_list')
+    def expr_list_or_nothing(self, p):
+        return p.expr_list
+
+    @_('enum')
+    def expr_list(self, p):
+        return p.enum
+
+    @_('expr')
+    def expr_list(self, p):
+        return [p.expr]
+
+    @_('enum COMMA expr')
+    def enum(self, p):
+        return p.enum + [p.expr]
+
+    @_('expr COMMA expr')
+    def enum(self, p):
+        return [p.expr0, p.expr1]
+
+    @_('identifier')
+    def expr(self, p):
+        return p.identifier
+
+    @_('constant')
+    def expr(self, p):
+        return p.constant
+
+    # 常量的规则
+    @_('NULL')
+    def constant(self, p):
+        return Constant(value=None)
+
+    @_('integer')
+    def constant(self, p):
+        return Constant(value=int(p.integer))
+
+    @_('string')
+    def constant(self, p):
+        return Constant(value=str(p[0]))
+
+    @_('identifier DOT identifier')
+    def identifier(self, p):
+        p.identifier0.parts += p.identifier1.parts
+        return p.identifier0
+
+    @_('id')
+    def identifier(self, p):
+        return Identifier(p[0])
+
+    @_('ID')
+    def id(self, p):
+        return p[0]
+
+    @_('quote_string',
+       'dquote_string')
+    def string(self, p):
+        return p[0]
+
+    @_('INTEGER')
+    def integer(self, p):
+        return int(p[0])
+
+    @_('QUOTE_STRING')
+    def quote_string(self, p):
+        return p[0].strip('\'')
+
+    @_('DQUOTE_STRING')
+    def dquote_string(self, p):
+        return p[0].strip('\"')
 
     @_('')
-    def expr(self, p):
+    def empty(self, p):
         pass
 
+    def error(self, p):
+        if p:
+            raise SyntaxError(f'Syntax error at token {p.type}: "{p.value}".')
+        else:
+            raise SyntaxError("Syntax error at end of file.")
+
+
+def query_parse(sql_stmt):
+    lexer = SQLLexer()
+    parser = SQLParser()
+    tokens = lexer.tokenize(sql_stmt)
+    return parser.parse(tokens)
+
+
+ast = query_parse('select a, b from t1')
+ast = query_parse('select a, b from t1 where a > c')
+ast = query_parse('select a, b from t1 where a > c order by b')
+ast = query_parse('select a, count(a) from t1 group by a where b > 100')
+print(ast)
+query_parse('select a, b from t1 order by b order by a')
